9cce5eec09b4ee0628960e76d654eed302d49566
unknown <damir777xolnazarov@gmail.com>
2024-06-05 23:29:37 +0500

    qweqweqwe

---
+++ src/components/GridPuzzle.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="puzzle-grid" :style="gridStyle">
+      <div
+        v-for="(tile, index) in tiles"
+        :key="index"
+        :class="['tile', { empty: tile === 0 }]"
+        :style="tileStyle"
+        @click="moveTile(index)"
+      >
+        {{ tile !== 0 ? tile : '' }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      gridSize: {
+        type: Number,
+        default: 3,
+      },
+    },
+    data() {
+      return {
+        tiles: this.shuffleTiles([...Array(this.gridSize * this.gridSize - 1).keys()].map(i => i + 1).concat(0)),
+      };
+    },
+    computed: {
+      gridStyle() {
+        return {
+          display: 'grid',
+          gridTemplateColumns: `repeat(${this.gridSize}, ${this.tileSize}px)`,
+          gridTemplateRows: `repeat(${this.gridSize}, ${this.tileSize}px)`,
+          gap: '5px',
+        };
+      },
+      tileStyle() {
+        return {
+          width: `${this.tileSize}px`,
+          height: `${this.tileSize}px`,
+        };
+      },
+      tileSize() {
+        switch (this.gridSize) {
+          case 3:
+            return 100;
+          case 4:
+            return 80;
+          case 5:
+            return 70;
+          default:
+            return 100;
+        }
+      },
+    },
+    methods: {
+      shuffleTiles(tiles) {
+        for (let i = tiles.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+        }
+        return tiles;
+      },
+      moveTile(index) {
+        const emptyIndex = this.tiles.indexOf(0);
+        const [row1, col1] = [Math.floor(index / this.gridSize), index % this.gridSize];
+        const [row2, col2] = [Math.floor(emptyIndex / this.gridSize), emptyIndex % this.gridSize];
+        
+        if (Math.abs(row1 - row2) + Math.abs(col1 - col2) === 1) {
+          [this.tiles[index], this.tiles[emptyIndex]] = [this.tiles[emptyIndex], this.tiles[index]];
+          if (this.isSolved()) {
+            alert('Congratulations! You solved the puzzle!');
+          }
+        }
+      },
+      isSolved() {
+        for (let i = 0; i < this.tiles.length - 1; i++) {
+          if (this.tiles[i] !== i + 1) return false;
+        }
+        return true;
+      },
+    },
+    watch: {
+      gridSize() {
+        this.tiles = this.shuffleTiles([...Array(this.gridSize * this.gridSize - 1).keys()].map(i => i + 1).concat(0));
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .tile {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
+    font-size: 24px;
+    cursor: pointer;
+  }
+  .tile.empty {
+    background-color: #ffffff;
+    cursor: default;
+  }
+  </style>
+  
--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,28 +1,71 @@
+<script setup>
+import PuzzleGrid from '../components/GridPuzzle.vue'
+</script>
 <template>
-    <div>
-    
+    <div class="games w-[100%]">
+        <div class="top flex flex-row justify-between items-center pt-[15px] px-[50px] w-[100%] h-[120px] flex-nowrap">
+            <img class="w-[70px] h-[36px]" src="../assets/PLAY.png" alt="">
+            <img class="w-[215px] h-[88px]" src="../assets/GAMES1.png" alt="">
+        </div>
+        <div class="center flex flex-row items-center px-[10px] w-[100%] h-[50px] justify-between">
+            <img class="h-[1px]" src="../assets/Line 2.png" alt="">
+            <img class="w-[50px] h-[22px]" src="../assets/AND.png" alt="">
+            <img class="h-[1px]" src="../assets/Line 2.png" alt="">
+        </div>
+        <div class="bottom px-[50px] flex flex-row items-center justify-between">
+            <span class="text-[25px]">
+                E <br> A <br> R <br> N <br>
+            </span>
+            <div class="coins relative">
+                <img src="../assets/C INS.png" alt="">
+                <img class="absolute w-[64px] top-[4px] left-[60px]" src="../assets/coin.png" alt="">
+                <img src="../assets/R.png" class="absolute top-[25px] left-[80px]" alt="">
+            </div>
+        </div>
+        <div class="game_frame px-[15px] pt-[35px]">
+            <div class="puzzle-game">
+                <div class="buttons">
+                    <button @click="changeGridSize(3)">3x3</button>
+                    <button @click="changeGridSize(4)">4x4</button>
+                    <button @click="changeGridSize(5)">5x5</button>
+                </div>
+                <PuzzleGrid :gridSize="gridSize" />
+            </div>
+        </div>
     </div>
 </template>
-
 <script>
 export default {
-    name: '',
-
+    components: {
+        PuzzleGrid,
+    },
     data() {
         return {
-
+            gridSize: 3,
         };
     },
-
-    mounted() {
-
-    },
-
     methods: {
-
+        changeGridSize(size) {
+            this.gridSize = size;
+        },
     },
 };
 </script>
-
 <style scoped>
+.puzzle-game {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.buttons {
+    margin-bottom: 20px;
+}
+
+button {
+    margin: 0 10px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
 </style>
